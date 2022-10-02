29db3192f02fe9d7d5e1958e90518c3a15b84d63
ForhadRobe <i.rforhad@gmail.com>
2022-10-02 19:04:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,122 +1,74 @@
 // import logo from './logo.svg';
-import { useEffect, useState } from 'react';
 import './App.css';
+const number = 5555;
+const singers = [{ name: 'MAhfuz', job: 'singer' },
+{ name: 'Eva apa', job: 'singer' },
+{ name: 'agun', job: 'singer' },
+{ name: 'subro', job: 'singer' }]
+const singerStyle = {
+  color: 'red',
+  backgroundColor: 'blue'
+}
 
 function App() {
+  const nayoks = ['rubel', 'sakib', 'kuber', 'bappa', 'jashim'];
   return (
     <div className="App">
-      <Timer></Timer>
-      <ExternalTodos></ExternalTodos>
-      {/* <Counter></Counter> */}
-      {/* <ExternalUsers></ExternalUsers> */}
+      {
+        nayoks.map(nayok => <li>Name:{nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name} nayika={singer.job}></Person>)
+      }
 
+      {/* <Person name={nayoks[0]} nayika="mousumi"></Person>
+      <Person name={nayoks[1]} nayika="mousumi"></Person>
+      <Person name={nayoks[2]} nayika="mousumi"></Person>
+      <Person name={nayoks[3]} nayika="mousumi"></Person>
+      <Person name="sakib" nayika="shisir"></Person>
+      <Person name="kuber" nayika="kapila"></Person> */}
+      <h5>Ekhane o lekha jay</h5>
+      <Friend name="tamim" phone="0170"></Friend>
+      <Friend name="mushfiq" phone="0180"></Friend>
+
+      {/* <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello dude,React is coming..</h3>
+        </div>
+        <div className="music">
+          <p> Name:{2222 + number}</p>
+          <p style={singerStyle}> Name:{singer.name} Job:{singer.job}</p>
+          <p style={{ color: 'blue', backgroundColor: 'yellow' }}> Name:{singer2.name} Job:{singer2.job}</p>
+        </div> */}
     </div>
   );
 }
-/* function ExternalUsers() {
-  const [users, setUsers] = useState([]);
-  // const myfunc=()=>{}
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      // .then(data => console.log(data))
-      .then(data => setUsers(data))
-
-  }, [])
+function Person(props) {
+  // console.log(props);
   return (
-    <div>
-      <h1>ExternalUsers</h1>
-      <p>{users.length}</p>
-      {
-        users.map(user => <li>{user.name}</li>)
-      }
-      {
-        users.map(user => <Users name={user.name} email={user.email} id={user.id}></Users>)
-      }
-    </div>
-  )
-} */
-/* function Users(props) {
-  return (
-    <div style={{ border: '2px solid red', margin: '20px', borderRadius: '50px', padding: '20px' }}>
-      <h2>Name:{props.name}</h2>
-      <p>Email:{props.email}</p>
-      <p><small>Id:{props.id}</small></p>
-    </div>
-  )
-} */
-
-function ExternalTodos() {
-  const [todos, setTodos] = useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(res => res.json())
-      // .then(data => console.log(data))
-      .then(data => setTodos(data))
-  }, [])
-  return (
-    <div>
-      <h2>ExternalTodos</h2>
-      <p>Total Todos:{todos.length}</p>
-      {
-        // todos.map(todo => <li>{todo.id}</li>)
-      }
-      {
-        todos.map(todo => <Todos name={todo.title} id={todo.id}></Todos>)
-      }
-    </div>
-  )
-}
-function Todos(props) {
-  return (
-    <div className='todos'>
-      <h2>Title:{props.name}</h2>
-      <p>Id:{props.id}</p>
+    <div className='person'>
+      <h1>Hero:{props.name}</h1>
+      <p>Name:{props.nayika}</p>
+      {/* <h1>sakib al hasan</h1>
+      <p>cricket</p> */}
     </div>
   )
 }
 
-
-function Timer() {
-  const [time, setTime] = useState(50);
-  const [time1, setTime1] = useState(100);
-  const increaseTime = () => {
-    const newTime = time + 1;
-    setTime(newTime);
-  }
-  const decreaseTime = () => setTime1(time1 - 1);
+function Friend(props) {
+  // console.log(props)
   return (
-    <div>
-      <h2>Time:{time} seconds   {time1} seconds</h2>
-      <button onClick={increaseTime} style={{ padding: '10px', backgroundColor: 'blueviolet', marginRight: '10px' }}>TimeIncrese</button>
-      <button onClick={decreaseTime} style={{ padding: '10px', backgroundColor: 'tomato' }}>TimeDecrese</button>
+    <div className='container'>
+      <h1>{props.name}</h1>
+      <p>{props.phone}</p>
+      {/* <h1>Name:Ajay Dev gun</h1>
+      <p>work:singum</p> */}
     </div>
+
   )
 }
-
-/* function Counter() {
-  const [count, setCount] = useState(33);
-  const [count1, setCount1] = useState(55);
-  // console.log(abc);
-  const increaseCount = () => setCount(count + 1);
-  const decreaseCount = () => setCount1(count1 - 1); */
-
-/* const increaseCount = () => {
-  const newCount = count + 1;
-  setCount(newCount);
-}
-const decreaseCount = () => {
-  const newCount1 = count1 - 1;
-  setCount1(newCount1);
-} */
-
-/* return (
-  <div>
-    <h1>Count:{count} {count1}</h1>
-    <button onClick={increaseCount}>Increase</button>
-    <button onClick={decreaseCount}>Decrease</button>
-  </div>
-)
-} */
 
 export default App;
